f0cfa4bf5ccd5574a1adfa281f97561c26b75e10
syedaayesha2309 <ayeshamasroor57@gmail.com>
2024-11-15 08:13:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMCmJtfNxFnZNS4cOi5EyPZ",
-      "include_colab_link": true
+      "include_colab_link":True
     },
     "kernelspec": {
       "name": "python3",
